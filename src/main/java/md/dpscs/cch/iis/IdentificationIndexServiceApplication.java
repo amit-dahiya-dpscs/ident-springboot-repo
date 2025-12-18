4d7fb8e11391dee7578e9ba996e059688a70d4fb
Dahiya <dahiyaax@dpscs.ad.icj.mdstate>
2025-12-18 12:31:46 -0500

    added latest changes

--- src/main/java/md/dpscs/cch/iis/IdentificationIndexServiceApplication.java
+++ src/main/java/md/dpscs/cch/iis/IdentificationIndexServiceApplication.java
@@ -3,11 +3,13 @@ package md.dpscs.cch.iis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class IdentificationIndexServiceApplication {
 
 	public static void main(String[] args) {
